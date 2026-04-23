ac99cddb7354ab0e8df98e8daa6580a7edce9893
Shredhaya <shredhayagoel@gmail.com>
2026-04-24 02:24:38 +0530

    Component-Form

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  name = '';
+  email = '';
+  password = '';
+
+  submitForm() {
+    const userData = {
+      name: this.name,
+      email: this.email,
+      password: this.password,
+    };
+
+    console.log(userData);
+  }
+}
