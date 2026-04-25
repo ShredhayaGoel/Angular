56ae93b5165464a786854609ed3317accb2825f7
Shredhaya <shredhayagoel@gmail.com>
2026-04-25 23:44:18 +0530

    Get and Set input field

--- src/app/counter/counter.ts
+++ src/app/counter/counter.ts
@@ -27,4 +27,25 @@ export class Counter {
 
     console.log('value', (event.target as HTMLInputElement).value);
   }
+  name = '';
+  displayname = '';
+  email = '';
+  getname(event: Event) {
+    const name = (event.target as HTMLInputElement).value;
+    this.name = name;
+    console.log(event);
+  }
+  showname(event: Event) {
+    this.displayname = this.name;
+  }
+  setName() {
+    this.name = 'Angular';
+  }
+  getemail(val: string) {
+    console.log(val);
+    this.email = val;
+  }
+  setemail() {
+    this.email = 'Angular';
+  }
 }
