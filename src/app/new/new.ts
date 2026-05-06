5cff60e9eeea888960707987ed6c7d32a8915faf
Shredhaya <shredhayagoel@gmail.com>
2026-05-06 23:28:38 +0530

    two way binding

--- src/app/new/new.ts
+++ src/app/new/new.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-new',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new.html',
   styleUrl: './new.css',
 })
 export class New {
   users = ['shubh', 'pratik', 'satyarth', 'satyarth'];
+  name = 'shubh';
+  userName: string = 'shubh';
 }
