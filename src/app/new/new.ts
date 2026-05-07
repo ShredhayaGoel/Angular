e57989daf3e36a779f123a2393bc33423b1595b4
Shredhaya <shredhayagoel@gmail.com>
2026-05-07 12:07:24 +0530

    TO-DO-LIST

--- src/app/new/new.ts
+++ src/app/new/new.ts
@@ -7,7 +7,20 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './new.css',
 })
 export class New {
-  users = ['shubh', 'pratik', 'satyarth', 'satyarth'];
+  users = ['shubh', 'pratik', 'satyarth', 'shubham'];
   name = 'shubh';
   userName: string = 'shubh';
+  task = 'meeting at 10am';
+  tasklist: { id: number; name: string; age: number }[] = [];
+  addtask() {
+    this.tasklist.push({
+      id: this.tasklist.length + 1,
+      name: this.task,
+      age: this.tasklist.length + 1,
+    });
+    console.log(this.tasklist);
+  }
+  deletetask(id: number) {
+    this.tasklist = this.tasklist.filter((items) => items.id !== id);
+  }
 }
